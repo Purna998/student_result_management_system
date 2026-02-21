6441eee1608f3126d17cb527c051e59283158b4d
Purna998 <acharyapurna2@gmail.com>
2026-02-21 12:21:34 +0545

    Automate superuser creation via environment variables to avoid paid shell

--- StudentResultManagement/build.sh
+++ StudentResultManagement/build.sh
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [[ $DJANGO_SUPERUSER_USERNAME ]]; then
+  echo "Creating superuser..."
+  python manage.py createsuperuser \
+    --no-input \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser already exists or creation failed."
+fi
